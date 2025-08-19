bb9837be6952fe5c02caecac079a951917f7dc91
vladick <mamrokhavlad@gmail.com>
2025-08-19 15:50:56 +0300

    constructor, towrite, toread implemented

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -1,2 +1,30 @@
 #include "RingBuffer.hpp"
 
+RingBuffer::RingBuffer(unsigned size)
+{
+	buffer = new char[size];
+	length = size;	
+	toread_idx = 0;
+	towrite_idx = 0;
+};
+
+
+void RingBuffer::towrite(char c)
+{
+	if(towrite_idx == length)
+		towrite_idx = 0;
+	
+	buffer[towrite_idx] = c;
+	towrite_idx++;
+
+};
+
+char RingBuffer::toread()
+{
+	if(toread_idx == length)
+		toread_idx = 0;
+	
+	char c = buffer[toread_idx];
+	toread_idx++;
+	return c;
+};
